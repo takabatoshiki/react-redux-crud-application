d8b835623f9cf94c4c4f5bad4d50c6eb260ba23a
takabatoshiki <takabatoshiki@takabariju-no-MacBook-Pro.local>
2019-03-04 09:20:31 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
+// jsx : js + xhl
 class App extends Component {
   render() {
-    return <div>Hello, world!</div>;
+    // const greeting = "Hi, Tom!";
+    // const dom = <h1 className="foo">{greeting}</h1>;
+    // return dom;
+    // return <input type="text" onChange={ () => {console.log("I am clicked.");
+    // } }  />;
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={ () => { console.log("I am clicked."); } } />
+      </React.Fragment>
+    )
   }
 }
 
+export default App;
+
+// js : Javascript
 // class App extends Component {
 //   render() {
 //     return React.createElement(
@@ -15,5 +29,3 @@ class App extends Component {
 //     );
 //   }
 // }
-
-export default App;
