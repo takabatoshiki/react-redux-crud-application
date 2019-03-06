691ad9727246f104ac26db5a0524cdd49360d530
takabatoshiki <takabatoshiki@takabariju-no-MacBook-Pro.local>
2019-03-06 09:09:21 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 // jsx : js + xhl
-class App extends Component {
-  render() {
-    // const greeting = "Hi, Tom!";
-    // const dom = <h1 className="foo">{greeting}</h1>;
-    // return dom;
-    // return <input type="text" onChange={ () => {console.log("I am clicked.");
-    // } }  />;
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={ () => { console.log("I am clicked."); } } />
-      </React.Fragment>
-    )
-  }
+// class App extends Component {
+//   render() {
+//     // const greeting = "Hi, Tom!";
+//     // const dom = <h1 className="foo">{greeting}</h1>;
+//     // return dom;
+//     // return <input type="text" onChange={ () => {console.log("I am clicked.");
+//     // } }  />;
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={ () => { console.log("I am clicked."); } } />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return(
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  ) 
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
